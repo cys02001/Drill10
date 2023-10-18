c8ca70579e0069faf884333ccae571be4bf691ba
yunseochoi(home) <jamie0730@naver.com>
2023-10-18 18:25:49 +0900

    drawing layer에 따라 분류하는 리스트 생성

--- game_world.py
+++ game_world.py
@@ -1,13 +1,26 @@
-objects = []
-def add_object(o):
-    objects.append(o)
+objects = [[],[]]
+
+
+def add_object(o, depth=0):
+    objects[depth].append(o)
+
+
 def update():
- for o in objects:
-     o.update()
+    for layer in objects:
+        for o in objects:
+            o.update()
+
+
 def render():
- for o in objects:
-     o.draw()
+    for layer in objects:
+        for o in objects:
+            o.draw()
+
 
 def remove_object(o):
-    objects.remove(o)
+    for layer in objects:
+        if o in layer:
+            layer.remove(o)
+            return
+    raise ValueError('Cannot delete non existing object')
 
